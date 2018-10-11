0931634869d9a354ad460c9131b3fc4d7f0dd690
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-11 19:24:02 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,12 @@ require 'yaml'
 def load_library(file_path)
   library= YAML.load_file(file_path) # yaml file
   translator = {"get_meaning" => {}, "get_emoticon" => {}} # empty hash
-  library.each do |meaning, emoticons| # go through yaml to get meanings and emotes
-    translator["get_meaning"][emoticons[1]] = meaning #keys inside get_meaning points to their meanings
-    translator["get_emoticon"][emoticons[0]] = emoticons[1] #keys inside get_emoticon points to their JP equivalents
+   # go through yaml to get meanings and emotes
+  library.each do |meaning, emoticons|
+    #keys inside get_meaning points to their meanings
+    translator["get_meaning"][emoticons[1]] = meaning 
+    #keys inside get_emoticon points to their JP equivalents
+    translator["get_emoticon"][emoticons[0]] = emoticons[1] 
   end
   translator
 end
